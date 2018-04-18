a846a066c569c1b841c773406b385aa87b19860b
Marcus Cheung <myc2131@tc.columbia.edu>
2018-04-18 22:16:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,27 +22,3 @@ function dec(a){
   return a -= 1
 }
 
-function makeInt(n){
-  parseInt('n', 10)
-  return n 
-}
-
-function makeInt(n){
-  parseInt('0 * 2328', 10)
-  return 0
-}
-
-function makeInt(n){
-  parseInt('sldkjflksjf', 10)
-  return NaN
-}
-
-function preserveDecimal(n) {
-  parseFloat('n')
-  return n
-}
-
-function preserveDecimal(n) {
-  parseFloat('n')
-  return NaN
-}
